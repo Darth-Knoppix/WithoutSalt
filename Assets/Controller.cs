d269b4c4168dba0556a904fa3834de4667b36127
Seth Corker <seth.corker@gmail.com>
2014-09-28 13:03:22 +1300

    - Controller support modified to allow analogue stick for movement - Code cleanup *Tested on Vita
    TODO: Interpolate when 'hopping' between planes

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -6,32 +6,52 @@ using System.Text;
 using System.Threading;
 
 public class Controller : MonoBehaviour {
-	private Vector3 jumpVelocity = new Vector3(0, 10, 0); //Jump power (Only change Y)
-	private Vector3 hop = new Vector3(0, 2, 0); //Jump power when switching planes (Only change Y)
-	private Vector3 speed = new Vector3(8, 0, 0); //Movement speed (Only change X)
+	private bool isFalling;
+	private bool PSM; 																				//Check if platform is PSM
+	private bool padAct, padJump, padPause, padThrow, padUp, padRight, padDown, padLeft; 			//Controls
+	private bool wait;
 
-	private bool isFalling = false;
+	private int waitCount;
+	private int waitTimer;
+	private int plane; 																				//Current plane
+	private static int[] planeZ; 																	//Only use planeZ[1-3]
 
-	private bool PSM = Application.platform == RuntimePlatform.PSM; //Check if platform is PSM
-	private bool padAct, padJump, padPause, padThrow, padUp, padRight, padDown, padLeft = false; //Controls
-    
-	private int plane = 2; //Current plane
-	private static int[] planeZ = new int[] {0, 2, 4, 6, 8}; //Only use planeZ[1-3]
+	private float movementX;
 
-	private bool wait = false;
-	private int waitCount = 0;
-	private int waitTimer = 20;
+	private Vector3 jumpVelocity; 																	//Jump power (Only change Y)
+	private Vector3 hop; 																			//Jump power when switching planes (Only change Y)
+	private Vector3 speed; 																			//Movement speed (Only change X)
 
 	// Use this for initialization
 	void Start () {
+		isFalling = false;
+		PSM = Application.platform == RuntimePlatform.PSM;
+		padLeft = false;
+		wait = false;
+		
+		waitCount = 0;
+		waitTimer = 20;
+		plane = 2;
+		planeZ = new int[] {0, 2, 4, 6, 8};
+		
+		movementX = 0.0f;
 
+		jumpVelocity = new Vector3 (0, 10, 0);
+		hop = new Vector3(0, 2, 0);
+		speed = new Vector3 (8, 0, 0);
 	}
 
+	void OnGUI() {
+		//Debugging Vertical and horizontal movement
+		GUI.Label(new Rect(10, 10, 100, 20), Input.GetAxis("Vertical").ToString());
+		GUI.Label(new Rect(10, 30, 100, 20), movementX.ToString());
+	}
+	
 	void OnCollisionStay (Collision col) {
-		if (col.collider.tag == "Ground") { //Player is touching the ground
+		if (col.collider.tag == "Ground") { 														//Player is touching the ground
 			isFalling = false;
 		}
-		if (col.collider.tag == "Tree") { //Player is walking into a tree
+		if (col.collider.tag == "Tree") { 															//Player is walking into a tree
 			print("YOU HIT THE TREE");
 		}
 	}
@@ -39,22 +59,14 @@ public class Controller : MonoBehaviour {
 
 	}
 	void OnCollisionExit (Collision col) {
-		if (col.collider.tag == "Ground") { //Player is no longer touching the ground
+		if (col.collider.tag == "Ground") { 														//Player is no longer touching the ground
 			isFalling = true;
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		padAct = Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.JoystickButton0); //E Key, Cross Button
-		padJump = Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.JoystickButton1) || Input.GetKey(KeyCode.JoystickButton4); //Space Key, Circle Button, Left Shoulder Button
-		padPause = Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.JoystickButton7); //Escape Key, Start Button
-		padThrow = Input.GetMouseButton(0) || Input.GetKey(KeyCode.JoystickButton2) || Input.GetKey(KeyCode.JoystickButton5); //Left Mousebutton, Square Button, Right Shoulder Button
-		padUp = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.JoystickButton8); //W Key, Up Key, Up Button
-		padRight = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.JoystickButton9); //D Key, Right Key, Right Button
-		padDown = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.JoystickButton10); //S Key, Down Key, Down Button
-		padLeft = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.JoystickButton11); //A Key, Left Key, Left Button
-
+		getInput ();
 		if (wait) {
 			++waitCount;
 			if(waitCount >= waitTimer) {
@@ -62,43 +74,63 @@ public class Controller : MonoBehaviour {
 				waitCount = 0;
 			}
 		}
-
-		if (padLeft) { //Move left
-			rigidbody.MovePosition(rigidbody.position - speed * Time.deltaTime);
-		} else if (padRight) { //Move right
-			rigidbody.MovePosition(rigidbody.position + speed * Time.deltaTime);
-		}
+		rigidbody.MovePosition (rigidbody.position + (speed * movementX * Time.smoothDeltaTime));
 
 		if (!isFalling && !wait) {
+			int currentPosition = plane;
 			if (padUp) { //Switch plane (backwards)
 				rigidbody.AddForce (hop, ForceMode.VelocityChange);
 
 				if (plane == 1) {
-					transform.position = new Vector3 (transform.position.x, transform.position.y, planeZ[2]);;
 					plane = 2;
 				} else if (plane == 2) {
-					transform.position = new Vector3 (transform.position.x, transform.position.y, planeZ[3]);
 					plane = 3;
 				}
 				wait = true;
-
 			}
 			if (padDown) { //Switch plane (forwards)
 				rigidbody.AddForce (hop, ForceMode.VelocityChange);
 
 				if (plane == 3) {
-					transform.position = new Vector3 (transform.position.x, transform.position.y, planeZ[2]);
 					plane = 2;
 				} else if (plane == 2) {
-					transform.position = new Vector3 (transform.position.x, transform.position.y, planeZ[1]);
 					plane = 1;
 				}
 				wait = true;
 			}
+
+			transform.position = new Vector3 (transform.position.x, transform.position.y, Mathf.Lerp(planeZ[currentPosition], planeZ[plane], 2));
+
 			if (padJump) { //Jump
 				rigidbody.AddForce (jumpVelocity, ForceMode.VelocityChange);
 				wait = true;
 			}
 		}
+		
+	}
+
+	void FixedUpdate(){
+				
+		}
+
+	void getInput(){
+		padAct 		= Input.GetKey(KeyCode.E) 		|| Input.GetKey(KeyCode.JoystickButton0); 	//E Key, Cross Button
+		padJump 	= Input.GetKey(KeyCode.Space) 	|| Input.GetKey(KeyCode.JoystickButton1) 
+													|| Input.GetKey(KeyCode.JoystickButton4); 	//Space Key, Circle Button, Left Shoulder Button
+		padPause 	= Input.GetKey(KeyCode.Escape) 	|| Input.GetKey(KeyCode.JoystickButton7); 	//Escape Key, Start Button
+		padThrow 	= Input.GetMouseButton(0) 		|| Input.GetKey(KeyCode.JoystickButton2) 
+													|| Input.GetKey(KeyCode.JoystickButton5); 	//Left Mousebutton, Square Button, Right Shoulder Button
+
+
+		float tempVert = Input.GetAxis("Vertical");												//Vertical movement for left analogue stick, arrow keys and w-s
+		padDown =  	tempVert <= -0.8 				|| Input.GetKey (KeyCode.JoystickButton10)
+													|| Input.GetKey(KeyCode.DownArrow);
+		padUp 	= 	tempVert >=  0.8				|| Input.GetKey (KeyCode.JoystickButton8)	
+													|| Input.GetKey(KeyCode.UpArrow);
+
+
+		movementX 	= Input.GetAxis("Horizontal");												//Horizontal movement for left analogue stick, arrow keys and a-d
+		if (Input.GetKey (KeyCode.JoystickButton11))	movementX = -1;
+		if (Input.GetKey (KeyCode.JoystickButton9))		movementX = 1;
 	}
 }
