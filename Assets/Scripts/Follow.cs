a1b0a5b28487bb18787c9eb721e97d60ef97b994
Henry Lawrence <h.lawrence@windowslive.com>
2014-09-30 14:49:15 +1300

    Added ForestLevel1.unity files

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+	public float offset;
+	public Transform target;
+	private Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newPosition.x = target.position.x;
+		newPosition.y = target.position.y + offset;
+		newPosition.z = ((target.position.z * 2) - 14) / 2;
+
+		transform.position = Vector3.Lerp (transform.position, newPosition, Time.deltaTime * 2);
+	}
+}
