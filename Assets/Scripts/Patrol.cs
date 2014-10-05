63cecd9e308c38390899a1b359e49d137de3e05a
greenbue <generyan.ebue@yahoo.com>
2014-10-05 22:03:03 +1300

    no message

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -10,30 +10,17 @@ public class Patrol : MonoBehaviour {
 	public bool pausePatrol = false;
 	public bool followHero = false;
 	private bool switchDir = false;
-<<<<<<< HEAD
 	public float moveLimit = 15;
 	private float origPos;
 	// Use this for initialization
 
 	void Start () {
 		origPos = transform.position.x;
-=======
-	private int move = 0;
-	public int moveLimit = 15;
-	// Use this for initialization
-
-	void Start () {
-		
->>>>>>> 93b0d16f50a2377370c6ed2faf39e7a8ebd1abc9
 	}
 
 	void OnCollisionEnter (Collision col)
 	{
-<<<<<<< HEAD
 
-=======
-		switchDir = !switchDir;
->>>>>>> 93b0d16f50a2377370c6ed2faf39e7a8ebd1abc9
 	}
 
 	// Update is called once per frame
@@ -41,23 +28,15 @@ public class Patrol : MonoBehaviour {
 		
 		if (switchDir == false) {
 			transform.Translate (Vector3.right * movementSpeed * Time.deltaTime);
-<<<<<<< HEAD
 			//move++;
 			if(transform.position.x >= origPos+moveLimit){
-=======
-			if(transform.position.x >= moveLimit){
->>>>>>> 93b0d16f50a2377370c6ed2faf39e7a8ebd1abc9
 				switchDir = true;
 			}
 			
 		} else {
 			transform.Translate (Vector3.left * movementSpeed * Time.deltaTime);
-<<<<<<< HEAD
 			//move--;
 			if(transform.position.x <= origPos-moveLimit){
-=======
-			if(transform.position.x <= -moveLimit){
->>>>>>> 93b0d16f50a2377370c6ed2faf39e7a8ebd1abc9
 				switchDir = false;
 			}
 		}
