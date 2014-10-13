c8e81f0ef8de92f94f7c2571b2a6a1abb9095d90
Seth Corker <seth.corker@gmail.com>
2014-10-14 11:25:04 +1300

    Checkpoint material change on activate Scorpion collision modified

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -6,26 +6,27 @@ public class Checkpoint : MonoBehaviour {
 	private int health;
 	public bool isActivated;
 	Controller player;
+	public Material[] materials;
 
 	// Use this for initialization
 	void Start () {
 		saltCost = 1;
 		player = (Controller) FindObjectOfType (typeof(Controller));
+		renderer.material = materials [isActivated ? 1 : 0];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		//renderer.material = isActivated ? activatedMat : normalMat;
+		renderer.material = materials [isActivated ? 1 : 0];
 	}
 
 	void OnTriggerEnter(Collider col){
 		if(col.gameObject.name == "Player")
 		{
-			print("OVER");
 			if(!isActivated){
 				player.setLastCheckpoint(this);
 				isActivated = true;
-				//play anim
 			}
 		}
 	}
